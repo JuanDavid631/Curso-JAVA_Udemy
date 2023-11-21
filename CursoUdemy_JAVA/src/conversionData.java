b6d248814bc3cb592c3571f50df8e074747f5f40
Juan David Montoya Lesmes <99360486+JuanDavid631@users.noreply.github.com>
2023-11-21 17:52:37 -0500

    UniversidadUdemy

--- CursoUdemy_JAVA/src/conversionData.java
+++ CursoUdemy_JAVA/src/conversionData.java
@@ -12,5 +12,17 @@ public class conversionData {
         System.out.print("Proporciona tu edad: ");
         edad = Integer.parseInt(consola.nextLine());
         System.out.println("Edad: " + edad);
+
+        //Convertir un tipo de dato a otro
+        var edadTexto = String.valueOf(10);
+        System.out.println("Edad en texto = " + edadTexto);
+
+        // Mostrar el indice que se asigna
+        var caracter = "Hola mundo".charAt(8);
+        System.out.println("Caracter: " + caracter);
+
+        System.out.print("Proporciona un caracter: ");
+        caracter = consola.nextLine().charAt(2);
+        System.out.println("Caracter: " + caracter);
     }
 }
