c8fab79d504f928568747d5c19202c6be3d9a5b1
Juan David Montoya Lesmes <99360486+JuanDavid631@users.noreply.github.com>
2023-11-24 00:53:26 -0500

    Metodos

---
+++ CursoUdemy_JAVA/src/Operaciones/Aritmetica.java
@@ -0,0 +1,13 @@
+package Operaciones;
+
+public class Aritmetica {
+  // Atributos de la clase
+    int atributo;
+    int atributo2;
+
+    // Metodo de la clase
+    public void sumar() {
+        int resultado = atributo + atributo2;
+        System.out.println("El resultado es: " + resultado);
+    }
+}
---
+++ CursoUdemy_JAVA/src/Operaciones/PruebaAritmetica.java
@@ -0,0 +1,10 @@
+package Operaciones;
+
+public class PruebaAritmetica {
+    public static void main(String[] args) {
+      Aritmetica aritmetica1 = new Aritmetica();
+      aritmetica1.atributo = 3;
+      aritmetica1.atributo2 = 2;
+      aritmetica1.sumar();
+    }
+}
