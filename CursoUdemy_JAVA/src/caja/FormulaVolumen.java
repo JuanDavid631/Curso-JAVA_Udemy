46f095d9e08360b9ab23f8fda76b6fad3e30126f
Juan David Montoya Lesmes <99360486+JuanDavid631@users.noreply.github.com>
2023-11-24 01:28:37 -0500

    Ejercicio_Clase-Metodos

--- CursoUdemy_JAVA/src/caja/FormulaVolumen.java
+++ CursoUdemy_JAVA/src/caja/FormulaVolumen.java
@@ -1,13 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package caja;
 
-/**
- *
- * @author mjuan
- */
 public class FormulaVolumen {
-    
+  public static void main(String[] args) {
+    // Creacion de un objeto
+    Volumen caja = new Volumen();
+    caja.ancho = 3;
+    caja.alto = 2;
+    caja.profundo = 6;
+    int resultado = caja.calcularVolumen();
+    System.out.println("El volumen de la caja es: " + resultado);
+
+    Volumen caja2 = new Volumen(2, 4, 6);
+    System.out.println("El volumen de la caja con argumentos es: " + caja2.calcularVolumen());
+  }
 }
--- CursoUdemy_JAVA/src/caja/Volumen.java
+++ CursoUdemy_JAVA/src/caja/Volumen.java
@@ -1,13 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package caja;
 
-/**
- *
- * @author mjuan
- */
 public class Volumen {
-    
+    int ancho;
+    int alto;
+    int profundo;
+
+    public Volumen() {
+        System.out.println("Ejecutando constructor vacio");
+    }
+
+    public Volumen(int ancho, int alto, int profundo) {
+        this.ancho = ancho;
+        this.alto = alto;
+        this.profundo = profundo;
+        System.out.println("Ejecutando constructor con argumentos");
+    }
+
+    public int calcularVolumen() {
+        return this.ancho * this.alto * this.profundo;
+    }
 }
