c6e579603cdb96e72c1ebd922aca694b8f076af0
Juan David Montoya Lesmes <99360486+JuanDavid631@users.noreply.github.com>
2023-11-24 01:19:59 -0500

    Metodos

--- CursoUdemy_JAVA/src/Operaciones/PruebaAritmetica.java
+++ CursoUdemy_JAVA/src/Operaciones/PruebaAritmetica.java
@@ -2,7 +2,7 @@ package Operaciones;
 
 public class PruebaAritmetica {
     public static void main(String[] args) {
-      Aritmetica aritmetica1 = new Aritmetica();
+      Aritmetica aritmetica1 = new Aritmetica(); // Se crea el objeto de tipo Aritmetica y constructor vacio
       aritmetica1.atributo = 3;
       aritmetica1.atributo2 = 2;
       aritmetica1.sumar();
@@ -12,5 +12,24 @@ public class PruebaAritmetica {
 
       resultado = aritmetica1.sumarConArgumentos(5, 8);
       System.out.println("El resultado usando argumentos es: " + resultado);
+
+      System.out.println("Arithmetic1: " + aritmetica1.atributo);
+      System.out.println("Arithmetic2: " + aritmetica1.atributo2);
+
+      Aritmetica aritmetica2 = new Aritmetica(5, 8);
+      System.out.println("Arithmetic1: " + aritmetica2.atributo);
+      System.out.println("Arithmetic2: " + aritmetica2.atributo2);
+      miMetodo();
+
+      // Variables locales
+      var number = 10;
+      var number2 = 5;
+    }
+
+    public static void miMetodo() {
+      // Variables locales
+      var number = 10; // No se puede acceder a esta variable desde el main porque es local a este metodo
+      var number2 = 5;
+      System.out.println("\nOtro metodo");
     }
 }
