475184c574482e06a749312496fce1131a31b1b8
Juan David Montoya Lesmes <99360486+JuanDavid631@users.noreply.github.com>
2023-11-25 23:55:23 -0500

    Paquetes

---
+++ CursoUdemy_JAVA/src/mx/com/globalmentoring/Utileria.java
@@ -0,0 +1,7 @@
+package mx.com.globalmentoring;
+
+public class Utileria {
+    public static void imprimir(String s) {
+        System.out.println("Dato = " + s);
+    }
+}
---
+++ CursoUdemy_JAVA/src/test/TestUtileria.java
@@ -0,0 +1,12 @@
+package test;
+
+import mx.com.globalmentoring.Utileria;
+import static mx.com.globalmentoring.Utileria.imprimir;
+
+public class TestUtileria {
+  public static void main(String[] args) {
+    Utileria.imprimir("Saludos");
+    imprimir("Adios");
+    mx.com.globalmentoring.Utileria.imprimir("Hasta luego");
+  }
+}
