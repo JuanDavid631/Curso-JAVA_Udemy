04a78e09d0438283701c2fa907a521543696955a
Juan David Montoya Lesmes <99360486+JuanDavid631@users.noreply.github.com>
2023-12-14 22:18:34 -0500

    Modificadores Acceso

---
+++ CursoUdemy_JAVA/src/modificadoresAcceso/Clase1.java
@@ -0,0 +1,13 @@
+package modificadoresAcceso;
+
+public class Clase1 {
+  public String atributoPublico = "Valor atributo publico";
+
+  public Clase1() {
+    System.out.println("Constructor publico");
+  }
+
+  public void metodoPublico() {
+    System.out.println("Metodo publico");
+  }
+}
