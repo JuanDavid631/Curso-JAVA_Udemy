db3952b35054a7031cae67753b7650036c170296
Juan David Montoya Lesmes <99360486+JuanDavid631@users.noreply.github.com>
2024-03-13 21:44:27 -0500

    Listado Personas

---
+++ Java Aplicaciones del Mundo Real/Listado_Personas/src/main/java/com/mycompany/listado_personas/Listado_Personas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.listado_personas;
+
+/**
+ *
+ * @author mjuan
+ */
+public class Listado_Personas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
