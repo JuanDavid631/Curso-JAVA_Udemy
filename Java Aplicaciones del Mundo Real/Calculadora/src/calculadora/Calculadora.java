7488dbb40a7d478a476151c115c510722a5fc70b
Juan David Montoya Lesmes <99360486+JuanDavid631@users.noreply.github.com>
2024-03-13 21:28:32 -0500

    Exceptions

--- Java Aplicaciones del Mundo Real/Calculadora/src/calculadora/Calculadora.java
+++ Java Aplicaciones del Mundo Real/Calculadora/src/calculadora/Calculadora.java
@@ -5,45 +5,53 @@ import java.util.Scanner;
 public class Calculadora {
     public static void main(String[] args) {
         Scanner consola = new Scanner(System.in);
-        System.out.println("**** Aplicacion Calculadora ****");
-        // Construccion del menu
-        var operacion = menu();
-        if (operacion >= 1 && operacion <= 4) {
-            // Pedir valores usuario
-            System.out.println("Proporciona valor Numero 1: ");
-            var operando1 = Integer.parseInt(consola.nextLine());
-            System.out.println("Proporciona valor Numero 2: ");
-            var operando2 = Integer.parseInt(consola.nextLine());
-            switch (operacion) {
-                case 1 -> {
-                    var sumaFinal = suma(operando1, operando2);
-                    System.out.println("Resultado de la suma: " + sumaFinal);
-                }
+        while (true) {
+            System.out.println("**** Aplicacion Calculadora ****");
+            // Construccion del menu
+            try {
+                var operacion = menu();
+                if (operacion >= 1 && operacion <= 4) {
+                    // Pedir valores usuario
+                    System.out.println("Proporciona valor Numero 1: ");
+                    var operando1 = Integer.parseInt(consola.nextLine());
+                    System.out.println("Proporciona valor Numero 2: ");
+                    var operando2 = Integer.parseInt(consola.nextLine());
+                    switch (operacion) {
+                        case 1 -> {
+                            var sumaFinal = suma(operando1, operando2);
+                            System.out.println("Resultado de la suma: " + sumaFinal);
+                        }
 
-                case 2 -> {
-                    var restaFinal = resta(operando1, operando2);
-                    System.out.println("Resultado de la resta: " + restaFinal);
-                }
+                        case 2 -> {
+                            var restaFinal = resta(operando1, operando2);
+                            System.out.println("Resultado de la resta: " + restaFinal);
+                        }
 
-                case 3 -> {
-                    var multiplicacionFinal = multiplicacion(operando1, operando2);
-                    System.out.println("Resultado de la multiplicacion: " + multiplicacionFinal);
-                }
+                        case 3 -> {
+                            var multiplicacionFinal = multiplicacion(operando1, operando2);
+                            System.out.println("Resultado de la multiplicacion: " + multiplicacionFinal);
+                        }
 
-                case 4 -> {
-                    var divisionFinal = division(operando1, operando2);
-                    System.out.println("Resultado de la division: " + divisionFinal);
-                }
+                        case 4 -> {
+                            var divisionFinal = division(operando1, operando2);
+                            System.out.println("Resultado de la division: " + divisionFinal);
+                        }
 
-                default -> {
+                        default -> {
+                            System.out.println("Operacion Numero " + operacion + " erronea");
+                        }
+                    }
+
+                } else if (operacion == 5) {
+                    System.out.println("Hasta luego ...");
+                    break;
+                } else {
                     System.out.println("Operacion Numero " + operacion + " erronea");
                 }
+                System.out.println();
+            } catch (Exception e) {
+                System.out.println("Ocurrio un error " + e.getMessage());
             }
-
-        } else if (operacion == 5) {
-            System.out.println("Hasta luego ...");
-        } else {
-            System.out.println("Operacion Numero " + operacion + " erronea");
         }
     }
 
